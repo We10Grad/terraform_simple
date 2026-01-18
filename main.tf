34881ac7895749a94077dedc6000e8428b09e80c
We10Grad <jonzeldenrust@gmail.com>
2026-01-18 14:06:56 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,16 +17,48 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "example" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  user_data = #!/bin/bash
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = "t2.micro"
+  user_data       = <<-EOF
+              #!/bin/bash
               sudo apt-get update -y
               sudo apt-get install nginx -y
               echo "Hello, World!" > /var/www/html/index.html
               systemctl start nginx
               systemctl enable nginx
+              EOF
+    security_groups = [aws_security_group.allow_http.name]
 
   tags = {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic and all outbound traffic"
+
+  tags = {
+    Name = "allow_http"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http_ipv4" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_http.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+#resource "aws_key_pair" "deployer" {
+#  key_name   = "deployer-key"
+#  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC3... user@hostname"
+#}
